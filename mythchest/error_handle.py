077a7d7fab6dbbb65f2e8207e1547e75d61dda12
MinaYu <minayu@MinaYude-MacBook-Air.local>
2020-11-12 15:36:21 +0800

    implement error handle

--- mythchest/error_handle.py
+++ mythchest/error_handle.py
@@ -2,6 +2,7 @@ import functools
 
 
 def exception_handle(function):
+    # TODO write comment and document
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
         try:
@@ -9,6 +10,6 @@ def exception_handle(function):
         except Exception as e:
             print(e)
             # TODO add log to alarm
-            # TODO implement feature that can be extended by other function, after error, do things.
+            # TODO implement feature that can be extended/used by other function, after error, do things.
             raise
     return wrapper
