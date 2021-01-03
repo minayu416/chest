85cf527932197a5852aa6ded8d6a99fbdf269ffa
Ming-Jung YU <minayu416@gmail.com>
2021-01-03 16:38:08 +0800

    rename project name and re-arranging structure of project

--- document/conf.py
+++ document/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'mythchest'
+project = 'AncientChest'
 copyright = '2020, minayu'
 author = 'minayu0416'
 
